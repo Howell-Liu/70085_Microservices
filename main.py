9f139e863d69cb04c743f1fbe9433a338e393b90
Joey Leung <joeyleung98@gmail.com>
2024-01-31 10:52:48 +0000

    fixed data to use json file

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from typing import Union
 from fastapi import FastAPI
 import json
-import books
 
 app = FastAPI()
 
 
 with open('data.json', 'r') as file:
-    books_data = json.load(file)
+    data = json.load(file)
+
 
 @app.get("/")
 def read_root():
@@ -15,9 +15,8 @@ def read_root():
 
 
 @app.get("/books/{book_id}")
-def read_item(book_id: int): #, q: Union[str, None] = None):
-    if book_id >= len(books.books):
+def read_item(book_id: int):
+    if book_id >= len(data['books']):
         return "Index out of bounds"
     else:
-        return books.books[book_id-1]
-    #return {"book_id": book_id, "q": q}
+        return data['books'][book_id-1]
